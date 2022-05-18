4e6c1d43fd3dc009b370df905c07f4177d605229
Jalpa Modi <jalpamodi86@gmail.com>
2022-05-18 13:03:10 +0530

    project upload

---
+++ src/Package1/Class1.java
@@ -0,0 +1,16 @@
+package Package1;
+
+public class Class1 {void m1() {
+	System.out.println("jalpa");
+	System.out.println("techno");
+}
+public static void main(String[] args) {
+System.out.println("techno");
+System.out.println("credits");
+
+}
+
+}
+
+
+
